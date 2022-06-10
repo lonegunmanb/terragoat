67f71e28310a7f5eb1b5c1b4b4c5bfa4aeab613d
zjhe <hezijie@microsoft.com>
2022-06-10 15:47:39 +0800

    try add gitleaks check

--- getleaks.sh
+++ getleaks.sh
@@ -2,6 +2,6 @@
 
 echo $(pwd)
 ls
-docker run --rm -v $(pwd):/path zricethezav/gitleaks:latest detect --source="/path" --no-git -r /path/finding.json --redact
+docker run --rm --mount type=bind,source=${PWD},target=/path zricethezav/gitleaks:latest detect --source="/path" --no-git -r /path/finding.json --redact
 success=$?
 [ $success -eq 0 ] && exit 0 || ls && cat $(pwd)/finding.json && exit 1
